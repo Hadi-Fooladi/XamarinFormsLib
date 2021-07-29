9c4ced7344cb12d22f9c7cf2433d25333025ef56
Hadi Fooladi Talari <hadi.f7@gmail.com>
2021-07-29 11:39:28 -0400

    Update Ext.cs

--- Lib/Classes/Ext.cs
+++ Lib/Classes/Ext.cs
@@ -3,9 +3,9 @@ using Xamarin.Forms;
 
 namespace HaFT.Xamarin.Forms.Lib.Ext
 {
-    public static class __Ext__
-    {
-	    public static void AddClickHandler(this View V, Action Handler)
-		    => V.GestureRecognizers.Add(new TapGestureRecognizer { Command = new Command(Handler) });
+	public static class __Ext__
+	{
+		public static void AddClickHandler(this View V, Action Handler)
+			=> V.GestureRecognizers.Add(new TapGestureRecognizer { Command = new Command(Handler) });
 	}
 }
