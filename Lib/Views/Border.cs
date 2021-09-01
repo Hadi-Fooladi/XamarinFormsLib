021c057874af6b67f403f9719d5ad8ad9893a3ac
Hadi Fooladi Talari <hadi.f7@gmail.com>
2021-09-01 16:40:00 -0400

    v2.1.6 (New Properties for Border: BorderRadius and FillColor)

--- Lib/Views/Border.cs
+++ Lib/Views/Border.cs
@@ -8,8 +8,10 @@ namespace HaFT.Xamarin.Forms.Lib
 		private abstract class BP<PropertyType> : BPH<PropertyType, Border> { }
 
 		public static readonly BindableProperty
-			BorderWidthProperty = BP<double>.Create("BorderWidth", 0),
-			BorderColorProperty = BP<Color>.Create("BorderColor", Color.Transparent);
+			BorderWidthProperty = BP<double>.Create(nameof(BorderWidth), 0),
+			BorderRadiusProperty = BP<double>.Create(nameof(BorderRadius), 0),
+			BorderColorProperty = BP<Color>.Create(nameof(BorderColor), Color.Transparent),
+			FillColorProperty = BP<Color>.Create(nameof(FillColor), Color.Transparent);
 
 		public double BorderWidth
 		{
@@ -21,6 +23,18 @@ namespace HaFT.Xamarin.Forms.Lib
 		{
 			get => (Color)GetValue(BorderColorProperty);
 			set => SetValue(BorderColorProperty, value);
+		}
+
+		public Color FillColor
+		{
+			get => (Color)GetValue(FillColorProperty);
+			set => SetValue(FillColorProperty, value);
+		}
+
+		public double BorderRadius
+		{
+			get => (double)GetValue(BorderRadiusProperty);
+			set => SetValue(BorderRadiusProperty, value);
 		}
 		#endregion
 	}
