0cbcd1f331a7dc46e2d74d979cfbb87ab398c7bf
Hadi Fooladi Talari <hadi.f7@gmail.com>
2022-05-04 12:34:54 -0400

    New Project: ModalPageTest

---
+++ Tests/ModalPage/ModalPageTest/ModalPageTest/App.xaml.cs
@@ -0,0 +1,24 @@
+﻿namespace ModalPageTest
+{
+	public partial class App
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new MainPage();
+		}
+
+		protected override void OnStart()
+		{
+		}
+
+		protected override void OnSleep()
+		{
+		}
+
+		protected override void OnResume()
+		{
+		}
+	}
+}
