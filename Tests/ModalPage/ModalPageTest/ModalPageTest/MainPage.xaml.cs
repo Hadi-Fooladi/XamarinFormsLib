21c6b4cf254c4ea79e6f973e839915085e7b1256
Hadi Fooladi Talari <hadi.f7@gmail.com>
2022-05-04 12:45:15 -0400

    ModalPageTest (v1.0.1)

--- Tests/ModalPage/ModalPageTest/ModalPageTest/MainPage.xaml.cs
+++ Tests/ModalPage/ModalPageTest/ModalPageTest/MainPage.xaml.cs
@@ -1,4 +1,8 @@
-﻿namespace ModalPageTest
+﻿using System;
+using Xamarin.Forms;
+using HaFT.Xamarin.Forms;
+
+namespace ModalPageTest
 {
 	public partial class MainPage
 	{
@@ -6,5 +10,18 @@
 		{
 			InitializeComponent();
 		}
+
+		#region Event Handlers
+		private void Open_OnClicked(object Sender, EventArgs E)
+		{
+			var lbl = new Label
+			{
+				Text = "This is a test.",
+				FontSize = 32
+			};
+
+			ModalPage.FromView(lbl, "Modal Page").ShowAsync();
+		}
+		#endregion
 	}
 }
