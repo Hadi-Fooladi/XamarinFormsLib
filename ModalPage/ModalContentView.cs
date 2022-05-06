8332a62fbf31030883bd9c8c015cf1fd2fedd836
Hadi Fooladi Talari <hadi.f7@gmail.com>
2022-05-06 11:20:57 -0400

    ModalPage (v1.1.3)

--- ModalPage/ModalContentView.cs
+++ ModalPage/ModalContentView.cs
@@ -8,7 +8,14 @@ namespace HaFT.Xamarin.Forms
 		#region IModalView
 		public View View => this;
 
+		/// <summary>
+		/// Default implementation: Calls <see cref="Close"/> to close the page
+		/// </summary>
 		public virtual void HandleBackButton() { Close(); }
+
+		/// <summary>
+		/// Default implementation: Triggers the <see cref="Closed"/> event to close the page
+		/// </summary>
 		public virtual void Close() { Closed?.Invoke(this, EventArgs.Empty); }
 
 		public event EventHandler Closed;
