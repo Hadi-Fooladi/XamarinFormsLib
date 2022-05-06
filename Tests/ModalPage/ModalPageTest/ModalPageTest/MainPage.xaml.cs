64327299c2169ab49507720839970d98c47013bf
Hadi Fooladi Talari <hadi.f7@gmail.com>
2022-05-06 11:28:39 -0400

    ModalPageTest (v1.1.3)

--- Tests/ModalPage/ModalPageTest/ModalPageTest/MainPage.xaml.cs
+++ Tests/ModalPage/ModalPageTest/ModalPageTest/MainPage.xaml.cs
@@ -27,42 +27,21 @@ namespace ModalPageTest
 
 		private async void Notepad_OnClicked(object Sender, EventArgs E)
 		{
-			const string
-				ABS = "Abs",
-				REL = "Rel",
-				AUTO = "Auto";
+			var page = new ModalPage(new SampleNoteView()) { Padding = 0 };
 
-			var page = new ModalPage(new SampleNoteView());
-
-			{
-				const string DIR = "Width";
-				var mode = await select(DIR);
-				if (mode == AUTO)
-					page.SetAutoWidth();
-				else
-				{
-					int num = await getInt(DIR);
-					if (mode == ABS) page.SetAbsoluteWidth(num);
-					else page.SetRelativeWidth(num);
-				}
-			}
-
-			{
-				const string DIR = "Height";
-				var mode = await select(DIR);
-				if (mode == AUTO)
-					page.SetAutoHeight();
-				else
-				{
-					int num = await getInt(DIR);
-					if (mode == ABS) page.SetAbsoluteHeight(num);
-					else page.SetRelativeHeight(num);
-				}
-			}
+			await set("Width", page.SetAutoWidth, page.SetAbsoluteWidth, page.SetRelativeWidth);
+			await set("Height", page.SetAutoHeight, page.SetAbsoluteHeight, page.SetRelativeHeight);
 
 			await page.ShowAsync();
 
-			async Task<string> select(string title) => await DisplayActionSheet(title, null, null, ABS, REL, AUTO);
+			#region Nested Functions
+			const string
+				ABS = "Abs",
+				REL = "Rel",
+				AUTO = "Auto",
+				NONE = "None";
+			
+			async Task<string> select(string title) => await DisplayActionSheet(title, null, null, ABS, REL, AUTO, NONE);
 
 			async Task<int> getInt(string title)
 			{
@@ -73,6 +52,19 @@ namespace ModalPageTest
 						return num;
 				}
 			}
+			
+			async Task set(string title, Action auto, Action<double> abs, Action<double> rel)
+			{
+				var mode = await select(title);
+
+				switch (mode)
+				{
+				case NONE: break;
+				case AUTO: auto(); break;
+				default: (mode == ABS ? abs : rel)(await getInt(title)); break;
+				}
+			}
+			#endregion
 		}
 		#endregion
 	}
