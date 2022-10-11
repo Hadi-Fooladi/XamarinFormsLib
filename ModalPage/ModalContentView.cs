ff4f4eb2cc454af0283f668d04eb93ee08d21bed
Hadi Fooladi Talari <hadi.f7@gmail.com>
2022-10-11 14:00:43 -0400

    ModalPage (v1.2.4) [ModalContentView.IgnoreBackButton]

--- ModalPage/ModalContentView.cs
+++ ModalPage/ModalContentView.cs
@@ -5,13 +5,19 @@ namespace HaFT.Xamarin.Forms
 {
 	public class ModalContentView : ContentView, IModalView
 	{
+		public bool IgnoreBackButton { get; set; } = false;
+
 		#region IModalView
 		public View View => this;
 
 		/// <summary>
-		/// Default implementation: Calls <see cref="Close"/> to close the page
+		/// Default implementation: Calls <see cref="Close" /> to close the page (only if <see cref="IgnoreBackButton" /> is false)
 		/// </summary>
-		public virtual void HandleBackButton() { Close(); }
+		public virtual void HandleBackButton()
+		{
+			if (!IgnoreBackButton)
+				Close();
+		}
 
 		/// <summary>
 		/// Default implementation: Triggers the <see cref="Closed"/> event to close the page
